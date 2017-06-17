c2fb7f30d90d1840b9143712f2519ff9a98be0e6
osimic <obradsimic@yahoo.com>
2017-06-17 13:12:52 +0200

    minor changes

--- kalkulator/src/kalkulator/Expression.scala
+++ kalkulator/src/kalkulator/Expression.scala
@@ -10,25 +10,25 @@ object Expression {
   case class Prod(e1:Expr, e2:Expr) extends Expr
   
   
-  def show(e:Expr) : String = e match {
-  	case Number(n) => n.toString
-  	
-  	case Var(x, y) => x
-  	
-  	case Sum(e1, e2) => show(e1) + "+" + show(e2)
-  	
-  	case Prod(Sum(e1, e2), Sum(e3,e4)) =>
-  	    "("+show(Sum(e1,e2))+")"+"*"+"("+show(Sum(e3,e4))+")"
-  		
-  	case Prod(Sum(e1,e2), e3) =>
-  	    "("+show(Sum(e1,e2))+")"+"*"+ show(e3)
-  		
-  	case Prod(e3, Sum(e1,e2)) =>
-  	    show(e3) + "*"+"("+show(Sum(e1,e2))+")"
-  		
-  	case Prod(e1,e2) => show(e1)+"*"+show(e2)
-  	
-  	case _ => "???"
-  }                              
+//  def show(e:Expr) : String = e match {
+//  	case Number(n) => n.toString
+//  	
+//  	case Var(x, y) => x
+//  	
+//  	case Sum(e1, e2) => show(e1) + "+" + show(e2)
+//  	
+//  	case Prod(Sum(e1, e2), Sum(e3,e4)) =>
+//  	    "("+show(Sum(e1,e2))+")"+"*"+"("+show(Sum(e3,e4))+")"
+//  		
+//  	case Prod(Sum(e1,e2), e3) =>
+//  	    "("+show(Sum(e1,e2))+")"+"*"+ show(e3)
+//  		
+//  	case Prod(e3, Sum(e1,e2)) =>
+//  	    show(e3) + "*"+"("+show(Sum(e1,e2))+")"
+//  		
+//  	case Prod(e1,e2) => show(e1)+"*"+show(e2)
+//  	
+//  	case _ => "???"
+//  }                              
   
 }
